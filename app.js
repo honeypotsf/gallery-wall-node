ee3904faae78860e42cf378a1fea3f3765a21e59
cgabrielgarcia <carla@carlagabriel.com>
2017-04-09 20:16:25 -0700

    Get correct JSON

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const bodyParser = require('body-parser');
 const Promise    = require('bluebird');
 const request    = Promise.promisify(require('request'));
 const api_key    = process.env.ETSY_KEY;
-const etsyUrl = 'https://openapi.etsy.com/v2/shops/carlagabrielgarcia/listings/active.js?&includes=Images:1&api_key=' + api_key;
+const etsyUrl    = 'https://openapi.etsy.com/v2/shops/carlagabrielgarcia/listings/active?&includes=Images:1&api_key=' + api_key;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var port = process.env.PORT || 3000;
 
@@ -19,11 +24,13 @@ var router = express.Router();
 router.get('/etsy', function(req, res) {
     request(etsyUrl)
       .then(function (response) {
-        res.send(response.body);
+        var body = response.body;
+        res.send(body);
       });
 });
 
 app.use('/', router);
+
 
 app.listen(port);
 console.log('Server running on port ' + port);
